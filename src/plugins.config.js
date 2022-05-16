39b7918686c7483cc3d4c5b37e046682f727771f
Pavel Stratil <pavel@vaizard.org>
2022-05-16 20:11:53 +0200

    add core routes

--- src/plugins.config.js
+++ src/plugins.config.js
@@ -23,10 +23,66 @@ export default {
         "category": "skeleton"
     },
     {
-        "path": "/core/auth",
+        "path": "/core/status/jwt/fetch",
         "plugin": core,
         "category": "core"
-    }
+    },
+    {
+        "path": "/core/status/jwt/decode",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/status/auth",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/signin",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/signout",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/auth/users",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/auth/domains",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/auth/test/pass",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/auth/test/fail",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/health",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/routes/list",
+        "plugin": core,
+        "category": "core"
+    },
+    {
+        "path": "/core/routes/tree",
+        "plugin": core,
+        "category": "core"
+    },
+
 
     ]
 }
--- src/plugins/core.manifest.js
+++ src/plugins/core.manifest.js
@@ -8,3 +8,4 @@ export default {
     "label": "Enforce",
     "module": lazy(() => import(`./Core`)),
 }
+
