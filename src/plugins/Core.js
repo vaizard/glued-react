f75d8363c706dd452ab26705533e0ea349ff475f
Pavel Stratil <pavel@vaizard.org>
2022-05-23 18:16:45 +0200

    test datatables

--- src/plugins/Core.js
+++ src/plugins/Core.js
@@ -2,6 +2,7 @@ import React, {Suspense, lazy} from 'react';
 import AuthenticationContext from "../AuthenticationContext"
 import CircularProgress from "@mui/material/CircularProgress";
 import CenteredBox from "../tools/CenteredBox";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 const authEnforcePath = "https://gdev.industra.space/api/core/status/auth/v1"
 //const authEnforcePath = "https://10.146.149.186/api/core/status/auth/v1"
@@ -34,7 +35,26 @@ class Core extends React.Component {
              <CircularProgress />
           </CenteredBox>
         }
-	return <div><pre>{JSON.stringify(this.state.content, null, 2)}</pre></div>
+	
+    const rows: GridRowsProp = [
+      { id: 1, col1: 'Hello', col2: 'World' },
+      { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+      { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    ];
+
+    const columns: GridColDef[] = [
+      { field: 'col1', headerName: 'Column 1', width: 150 },
+      { field: 'col2', headerName: 'Column 2', width: 150 },
+    ];
+
+
+    return (
+        <div><pre>{JSON.stringify(this.state.content, null, 2)}</pre></div>
+        <div style={{ height: 300, width: '100%' }}><DataGrid rows={rows} columns={columns} /></div>
+     );
+
+
+
     }
 }
 
