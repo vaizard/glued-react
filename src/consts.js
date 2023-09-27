3b0a21096a6c417734df16a604d8e20f47ce9245
Pavel Stratil <pavel@vaizard.org>
2023-09-27 11:10:56 +0200

    update default consts.js

--- src/consts.js
+++ src/consts.js
@@ -6,11 +6,11 @@ if (process.env.NODE_ENV !== 'production') {
     endpoint = "https://gdev.industra.space";
     //endpoint = "https://fare.nemocnice.local";
     authenticatorOptions = {
-        tokenEndpoint: "https://id.industra.space/auth/realms/T1/protocol/openid-connect/token",
-        authorizationEndpoint: "https://id.industra.space/auth/realms/t1/protocol/openid-connect/auth",
-        endSessionEndpoint: "https://id.industra.space/auth/realms/t1/protocol/openid-connect/logout",
+        tokenEndpoint: "https://id.industra.space/auth/realms/industra/protocol/openid-connect/token",
+        authorizationEndpoint: "https://id.industra.space/auth/realms/industra/protocol/openid-connect/auth",
+        endSessionEndpoint: "https://id.industra.space/auth/realms/industra/protocol/openid-connect/logout",
         // logoutRedirectUri: "someuri", // Optional, by default the window.location. Here you can server-side cleanup, for example unset cookies, if your server set any.
-        clientId: "new-client-2",
+        clientId: "oidc-public",
         // redirectUri: "someUri" // Optional, by default the window.location. This is the page where authenticator.checkLogin() is called
     };
 
